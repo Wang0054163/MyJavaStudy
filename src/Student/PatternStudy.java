ffdcd10115734edd1519569c5b96039ddcc10ad2
Wang0054163 <3559862943@qq.com>
2026-02-06 21:35:21 +0800

    Day 5: 掌握嵌套循环逻辑，实现等腰三角形与直角三角形与菱形图形算法。

--- src/Student/PatternStudy.java
+++ src/Student/PatternStudy.java
@@ -9,8 +9,12 @@ public class PatternStudy {
 
         System.out.println("\n--- 2. 直角三角形 (不含空格逻辑) ---");
         drawRightTriangle(n);
+
+        System.out.println("\n--- 3. 菱形三角形 (不含空格逻辑) ---");
+        drawDiamond(n);
     }
-//    嵌套循环 (Nested Loops)：
+
+    //    嵌套循环 (Nested Loops)：
 //    外层循环负责“行”：跑一次就是一行。
 //    内层循环负责“列”：在这一行里横着打印空格或星号。
     // 等腰三角形：先空格，再星号
@@ -33,6 +37,34 @@ public class PatternStudy {
     public static void drawRightTriangle(int n) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        }
+    public static void drawDiamond(int n) {
+        // --- 第一部分：上半层（正三角形） ---
+        for (int i = 1; i <= n; i++) {
+            // 打印空格，将星号推向中心
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // 打印星号
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // --- 第二部分：下半层（倒三角形） ---
+        // 注意：i 从 n-1 开始，因为最中间那行不重复打
+        for (int i = n - 1; i >= 1; i--) {
+            // 打印空格，随着行数减少，空格反而增加
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // 打印星号
+            for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
